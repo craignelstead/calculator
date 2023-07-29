dc802c888786805f42400e05f746963a2023349e
craignelstead <ctnelst92@gmail.com>
2023-07-28 20:13:18 -0600

    Created blank formula template

---
+++ calcscript.js
@@ -0,0 +1,57 @@
+/*
+    Explain how script works here
+*/
+
+//Runs when program loads
+assignBtnListeners();
+
+//Adds event listeners to all buttons
+function assignBtnListeners(){
+
+}
+
+//Used to decide which operator to call. Called when equal key is pressed.
+function operate(){
+
+}
+
+//Checks if valid expression is written
+function checkExpression(){
+
+}
+
+//Addition operator
+function add(){
+
+}
+
+//Subtraction operator
+function subtract(){
+
+}
+
+//Multiplication operator
+function multiply(){
+
+}
+
+//Division operator
+function divide(){
+
+}
+
+//Called if division by zero is attempted
+function divideByZero(){
+
+}
+
+//Backspace button to delete most recent character
+function backspace(){
+
+}
+
+//Clears all memory
+function clearAll(){
+
+}
+
