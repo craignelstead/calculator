881748f6cdd30568f131e8c2821645323e036070
craignelstead <ctnelst92@gmail.com>
2023-07-28 21:32:18 -0600

    Num and operator buttons add text to screen.  Programmed clear and backspace buttons.

--- calcscript.js
+++ calcscript.js
@@ -18,17 +18,35 @@ function assignBtnListeners(){
 
 //Determines which button was pressed and how to proceed
 function checkButtonPressed(button){
-    alert(button.id);
+    //Determine button by id
+    switch(button.id) {
+        case 'btnEquals':
+            operate();
+            break;
+        case 'btnClear':
+            clearAll();
+            break;
+        case 'btnBackspace':
+            backspace();
+            break;
+        default:
+            updateScreen(button);
+    }
 }
 
 //Updates the calculator screen
-function updateScreen(){
-    
+function updateScreen(button){
+    const screenText = document.getElementById('screenText');
+    const buttonText = button.textContent.toString();
+
+    if(screenText.innerText.length <= 10)
+        {screenText.textContent += buttonText;} 
 }
 
 //Used to decide which operator to call. Called when equal key is pressed.
 function operate(){
-
+    //Checks if expression is valid before calculating
+    checkExpression();
 }
 
 //Checks if valid expression is written
@@ -63,11 +81,19 @@ function divideByZero(){
 
 //Backspace button to delete most recent character
 function backspace(){
-
+    const screenText = document.getElementById('screenText');
+    let screenContent = screenText.innerText.toString();
+    screenText.textContent = screenContent.slice(0, -1);
 }
 
 //Clears all memory
 function clearAll(){
+    const screenText = document.getElementById('screenText');
+    screenText.textContent = '';
+}
+
+//Round long numbers
+function roundNumber(){
 
 }
 
